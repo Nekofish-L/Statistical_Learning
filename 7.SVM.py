9d9e5628d1b5319cf266cfa587f328ba14021c33
slen99 <1558615303@qq.com>
2020-08-28 18:05:52 +0800

    add dataset iris.data

---
+++ 7.SVM.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+
+class SVM(object):
+    """
+    kernel: linear, poly, gauss
+    """
+    def __init__(self, kernel="linear"):
+        self.kernel = kernel
+
+    @classmethod
+    def __linear():
+        pass
+        
+
+    def fit(self, X_train, y):
+        pass
+
+    def predict(self, X):
+        pass
+
+
+if __name__ == "__main__":
+    svm = SVM()
+    
